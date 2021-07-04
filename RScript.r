edca174402a440c9abcb013ce0bed77ea99a43bd
Default user <adebola.1@osu.edu>
2021-07-04 16:06:35 +0000

    Commit

---
+++ RScript.r
@@ -0,0 +1,11 @@
+regrex<- read.csv("regrex1.csv")
+png('r_orig.png')
+plot(regrex, pch = 16, col = "blue")
+dev.off()
+
+lmRegrex = lm(x~y, data = regrex) #Create the linear regression
+png('r_lm.png')
+plot(regrex, pch = 16, col = "blue") #Plot the results
+abline(lmRegrex) #Add a regression line
+dev.off()
+
